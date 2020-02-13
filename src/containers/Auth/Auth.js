c3b4d28bf48ba75963657259817779d56d4c1a70
aronbergman <brgmn@icloud.com>
2020-02-13 21:13:01 +0300

    Add (Auth) | Added autorization component

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,11 +1,46 @@
-import React, {Component} from 'react';
+import React, {Component} from 'react'
+import classes from './Auth.module.scss'
+import {Button} from '../../components/UI/Button/Button'
+import {Input} from '../../components/UI/Input/Input'
 
 export default class Auth extends Component {
-    render() {
 
+    loginHandler = () => {
+    };
+
+    registerHandler = () => {
+    };
+
+    submitHandler = event => {
+        event.preventDefault()
+    };
+
+    render() {
         return (
-            <div>
-                <h1>Auth</h1>
+            <div className={classes.Auth}>
+                <div>
+                    <h1>Авторизация</h1>
+
+                    <form onSubmit={this.submitHandler} className={classes.AuthForm}>
+                        <Input label="Email"/>
+                        <Input label="Password"
+                        isInvalid="TEST"/>
+
+                        <Button
+                            type="success"
+                            onClick={this.loginHandler}
+                        >
+                            Войти
+                        </Button>
+
+                        <Button
+                            type="primary"
+                            onClick={this.registerHandler}
+                        >
+                            Зарегистрироваться
+                        </Button>
+                    </form>
+                </div>
             </div>
         )
     }
